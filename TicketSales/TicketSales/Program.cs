d0f485aaa265d959404285948e026cc8cf2424d2
Pacolele <e.hernesten@gmail.com>
2019-11-20 12:58:19 +0100

    lektion 2
    Startade basics, kan ändra mycket till nästa gång.

--- TicketSales/TicketSales/Program.cs
+++ TicketSales/TicketSales/Program.cs
@@ -1,10 +1,61 @@
 ﻿using System;
-
+using System.Collections.Generic;
 class Program
 {
+    private List<tickets> antColony = new List<tickets>();
     static void Main()
     {
-        Console.WriteLine("Hello and welcome how many tickets would u like?");
+        new Program().Run();
+    }
+
+    void Run()
+    {
+        Console.WriteLine("Hello welcome to this ticket booth here you can buy your tickets \nPlease enter 'help' for available commands\n\n");
+        while (true)
+        {
+            Console.WriteLine("Please enter your command\n>>>");
+            string input = Console.ReadLine();
+            if(input == "exit")
+            {
+                break;
+            }
+
+            else
+            {
+                InputCentre(input);
+            }
+        }
+       
+    }
+
+    void InputCentre(string input)
+    {
+        switch (input)
+        {
+            case "help":
+                Help();
+                break;
+
+            case "buy":
+                Buy();
+                break;
+
+            default:
+                Console.WriteLine("Please enter 'help' for available commands");
+                break;
+        }
+    }
+
+    private void Buy()
+    {
+        
+    }
+
+    private void Help()
+    {
+        Console.WriteLine("\nbuy - buy a ticket or multiple tickets\n" +
+            "temp - adad\n" +
+            "temp - aaaa\n");
     }
 }
 
---
+++ TicketSales/TicketSales/tickets.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+ class tickets
+{
+    public string Åldersgrupp;
+    public int Pengar;
+}
+
