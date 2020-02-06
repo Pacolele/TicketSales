85b374880452d3354b968d079513a5e8a015ebda
Pacolele <e.hernesten@gmail.com>
2020-02-06 22:50:21 +0100

    Vecka 5(finished)

--- TicketSales/TicketSales/Program.cs
+++ TicketSales/TicketSales/Program.cs
@@ -2,13 +2,15 @@
 using System.IO;
 using System.Collections.Generic;
 
-
 class Program
 {
     List<Item> items = new List<Item>();
     //List<Receipt> receipt = new List<Receipt>();
-    const string path = @"..\..\..\Receipt.txt";
+    const string receipt = @"..\..\..\Receipt.txt";
+    const string information = @"..\..\..\Information.txt";
     int id = 0;
+    int newSum = 0;
+    int RefundSum = 0;
 
     public Program()
     {
@@ -24,17 +26,19 @@ class Program
 
     void Run()
     {
+        UpdateRefunds();
+        UpdateInfo();
+        SaveInfo();
         UpdateID();
         Console.WriteLine("Hello welcome to this ticket booth here you can buy your tickets \nPlease enter 'help' for available commands\n\n");
         while (true)
         {
-            Console.WriteLine("\nBooth\n>>>");
+            Console.WriteLine("\nTicket booth\n>>>");
             string input = Console.ReadLine();
             if (input == "exit")
             {
                 break;
             }
-
             else
             {
                 InputCentre(input);
@@ -45,10 +49,41 @@ class Program
 
     private void UpdateID()
     {
-        string[] lines = File.ReadAllLines(path);
+        string[] lines = File.ReadAllLines(receipt);
         id = lines.Length / 4;
     }
 
+    private void UpdateInfo()
+    {
+        newSum = 0;
+        string[] lines = File.ReadAllLines(receipt);
+        if (lines.Length < 2)
+            return;
+        for (int i = 0; i < lines.Length; i+=5)
+        {
+            string[] substrings = lines[i].Split("%");
+            if (substrings.Length == 2)
+                continue;
+            string money = lines[i+4];
+            newSum += int.Parse(money.Substring(1));
+        }
+
+    }
+    private void UpdateRefunds()
+    {
+        RefundSum = 0;
+        string[] lines = File.ReadAllLines(receipt);
+        if (lines.Length < 2)
+            return;
+        for (int i = 0; i < lines.Length; i += 5)
+        {
+            string[] substrings = lines[i].Split("%");
+            if (substrings.Length == 1)
+                continue;
+            string money = lines[(i + 4)];
+            RefundSum += int.Parse(money.Substring(1));
+        }
+    }
     void InputCentre(string input)
     {
         switch (input)
@@ -76,41 +111,47 @@ class Program
     {
         Console.WriteLine("You want to refund? \n\n" + "Please enter your order Id to refund");
         string input = Console.ReadLine();
-        string[] lines = File.ReadAllLines(path);
-        for(int i = 0; i < lines.Length; i++)
+        string[] lines = File.ReadAllLines(receipt);
+        for (int i = 0; i < lines.Length; i++)
         {
             string[] strings = lines[i].Split("$");
 
             if (strings[0] == "ID" && strings[1] == input)
             {
-                lines[i] += "$Refunded";
+                lines[i] += "%Refunded";
             }
 
         }
-        File.WriteAllLines(path, lines);
+        File.WriteAllLines(receipt, lines);
+        UpdateRefunds();
+        SaveInfo();
     }
 
     public void Buy()
     {
         int sum = 0;
         int amountofItems = 0;
-        StreamWriter sw = new StreamWriter(path, true);
+        StreamWriter sw = new StreamWriter(receipt, true);
         sw.WriteLine("ID$" + id);
+
         Console.WriteLine($"Your order ID:{id}");
-        
+
         foreach (Item i in items)
         {
             Console.WriteLine("how many " + i.type + " tickets would you like?");
             amountofItems = CheckIfInt();
             sum += i.Price * amountofItems;
             sw.WriteLine(i.type + " $" + i.Price * amountofItems);
-            
+
         }
         sw.WriteLine("$" + sum);
         sw.Close();
         Console.WriteLine($"Your total value is: {sum}$");
 
         id++;
+
+        UpdateInfo();
+        SaveInfo();
     }
 
     int CheckIfInt()
@@ -127,81 +168,19 @@ class Program
             }
         }
     }
-
-
-    // Problem
-
-    /*
-        Console.WriteLine("Are you happy with your order Y/N");
-        if (Console.ReadLine().ToUpper() == "Y" )
-        {
-            using (StreamWriter sw = new StreamWriter(path))
-            {
-                foreach (Item i in items)
-                    sw.WriteLine(i.type + " $" + i.Price * boughtAmount);
-            }
-            break;
-        } */
-
-
-    /*
-               for (int i = 0; i < items.Count; i++)
-               {
-                   if (ticketType.Equals(items[i].type))
-                   {
-                       Console.WriteLine("\nPlease enter how many tickets you would like.");
-                       boughtAmount = int.Parse(Console.ReadLine());
-
-                       sum += items[i].Price * boughtAmount;
-                   }
-
-               }*/
-
-    //public void AddChildTicket()
-    //{
-    //    try
-    //    {
-    //        Console.WriteLine("How many children tickets would u like?");
-    //        int amountC = int.Parse(Console.ReadLine());
-    //        int totalC = 25 * amountC;
-
-    //    }
-    //    catch
-    //    {
-    //        Console.WriteLine("\nTry entering a number, please");
-    //    }
-    //}
-    //public void AddAdultTicket()
-    //{
-    //    try
-    //    {
-    //       Console.WriteLine("How many adult tickets would u like?");
-    //       int amountA = int.Parse(Console.ReadLine());
-    //        int totalvalue = 100 * amountA;
-    //    }
-    //    catch
-    //    {
-    //        Console.WriteLine("\nTry writing a number, please");
-    //    }
-    //}
-    //public void AddSeniorTicket()
-    //{
-    //    try
-    //    {
-    //        Console.WriteLine("How many senior tickets would u like?");
-    //        int amountS = int.Parse(Console.ReadLine());
-    //        int totalvalue = 75 * amountS;
-    //    }
-    //    catch
-    //    {
-    //        Console.WriteLine("\nTry writing a number, please");
-    //    }
-    //}
     private void Help()
     {
         Console.WriteLine("\nbuy - buy tickets \n" +
             "refund - refunds tickets\n" +
             "exit - get out of here\n");
+    }
+
+    void SaveInfo()
+    {
+        string[] strings = new string[2];
+        strings[0] = "Total made money$" + newSum;
+        strings[1] = "Refunded money$" + RefundSum;
+        File.WriteAllLines(information, strings);
     }
 }
 
